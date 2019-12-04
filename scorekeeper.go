f515eda2fbafe029209e2eff55cc9732380ee359
Mauro Reverter <revertermauro@gmail.com>
2019-12-04 17:28:36 -0300

    Added hands on solution

--- scorekeeper.go
+++ scorekeeper.go
@@ -1,19 +1,17 @@
 package scorekeeper
 
 type Scorer struct {
-	score int
+	score map[string]int
 }
 
 func NewScorer() Scorer {
-	return Scorer{score: 0}
+	return Scorer{score: make(map[string]int)}
 }
 
-func (s *Scorer) Get() int {
-	return s.score
+func (s *Scorer) Get(player string) int {
+	return s.score[player]
 }
 
-func (s *Scorer) Add(points int) {
-	s.score += points
+func (s *Scorer) Add(points int, player string) {
+	s.score[player] += points
 }
-
-
--- scorekeeper_test.go
+++ scorekeeper_test.go
@@ -8,18 +8,31 @@ import (
 
 func TestGetInitialScore(t *testing.T) {
 	scorer := scorekeeper.NewScorer()
-	assert.Equal(t, 0, scorer.Get())
+	assert.Equal(t, 0, scorer.Get("Home"))
 }
 
 func TestAddPointsToScore(t *testing.T) {
 	scorer := scorekeeper.NewScorer()
-	scorer.Add(5)
-	assert.Equal(t, 5, scorer.Get())
+	scorer.Add(5, "Home")
+	assert.Equal(t, 5, scorer.Get("Home"))
 }
 
 func TestAddPointsMultipleTimesToScore(t *testing.T) {
 	scorer := scorekeeper.NewScorer()
-	scorer.Add(5)
-	scorer.Add(5)
-	assert.Equal(t, 10, scorer.Get())
+	scorer.Add(5, "Home")
+	scorer.Add(5, "Home")
+	assert.Equal(t, 10, scorer.Get("Home"))
+}
+
+func TestGetInitialScoresForMultiplePlayers(t *testing.T) {
+	scorer := scorekeeper.NewScorer()
+	assert.Equal(t, 0, scorer.Get("Home"))
+	assert.Equal(t, 0, scorer.Get("Away"))
+}
+
+func TestAddPointsToOnePlayerAndNotOthers(t *testing.T) {
+	scorer := scorekeeper.NewScorer()
+	scorer.Add(10, "Home")
+	assert.Equal(t, 10, scorer.Get("Home"))
+	assert.Equal(t, 0, scorer.Get("Away"))
 }
